64c632a9147a95368fa0d08cb0f4a1f6f13a5e3d
Victor Mwania <victormwnza@gmail.com>
2018-10-07 20:19:45 +0300

    made the /whoami endpint

--- server.js
+++ server.js
@@ -1,30 +1,36 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
+var parser = require('ua-parser-js')
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionSuccessStatus: 200})); 
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-// listen for requests :)
+app.enable('trust proxy')
+
+app.get('/whoami', function(req, res) {
+  const ip = req.headers['x-forwarded-for'] ||
+  req.connection.remoteAddress ||
+  req.socket.remoteAddress ||
+  req.connection.socket.remoteAddress;
+  const language = req.get('accept-language')
+  const software = parser(req.headers['user-agent'])
+  res.json({
+    "ipaddress": ip,
+    "language": language.split(',')[0],
+    "software": JSON.stringify(software)
+  })
+})
+
 var listener = app.listen(process.env.PORT || 4400, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
